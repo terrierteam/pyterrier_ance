0ee31970a4b52ea862e511e84ceac36c7c03a019
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2022-01-23 15:15:35 +0000

    added ance prf and other pipeline components

--- pyterrier_ance/__init__.py
+++ pyterrier_ance/__init__.py
@@ -1,6 +1,8 @@
+from cgitb import text
+from re import L
 from pyterrier.datasets import Dataset
 from typing import Union
-from pyterrier.transformer import TransformerBase
+from pyterrier import Transformer as TransformerBase
 
 
 def _load_model(args, checkpoint_path):
@@ -131,8 +133,9 @@ import numpy as np
 
 class ANCERetrieval(TransformerBase):
 
-    def __init__(self, checkpoint_path=None, index_path=None, cpu_index=None, passage_embedding2id = None, docid2docno=None, num_results=100):
+    def __init__(self, checkpoint_path=None, index_path=None, cpu_index=None, passage_embedding2id = None, docid2docno=None, num_results=100, query_encoded=False):
         self.args = type('', (), {})()
+        self.query_encoded = query_encoded
         args = self.args
         args.local_rank = -1
         args.model_type = 'rdot_nll'
@@ -202,27 +205,36 @@ class ANCERetrieval(TransformerBase):
         return "ANCE"
 
     def transform(self, topics):
+        assert "docno" not in topics.columns
         from pyterrier import tqdm
-        queries=[]
+        
         qid2q = {}
-        for q, qid in zip(topics["query"].to_list(), topics["qid"].to_list()):
-            passage = self.tokenizer.encode(
-                q,
-                add_special_tokens=True,
-                max_length=self.args.max_seq_length,
-            )
-                
-            passage_len = min(len(passage), self.args.max_query_length)
-            input_id_b = pad_input_ids(passage, self.args.max_query_length)
-            queries.append([passage_len, input_id_b])
-            qid2q[qid] = q
+      
+        if self.query_encoded:
+            assert "query_emb" in topics.columns
+            dev_query_embedding = torch.stack(topics.query_emb.values.tolist()).numpy()
+            qid2q = { row.qid : row.query for row in topics.itertuples()  }
+
+        else:
+            queries=[]
+            for q, qid in zip(topics["query"].to_list(), topics["qid"].to_list()):
+                passage = self.tokenizer.encode(
+                    q,
+                    add_special_tokens=True,
+                    max_length=self.args.max_seq_length,
+                )
+                    
+                passage_len = min(len(passage), self.args.max_query_length)
+                input_id_b = pad_input_ids(passage, self.args.max_query_length)
+                queries.append([passage_len, input_id_b])
+                qid2q[qid] = q
+            
+            print("***** inference of %d queries *****" % len(queries))
+            dev_query_embedding, dev_query_embedding2id = StreamInferenceDoc(self.args, self.model, GetProcessingFn(
+                self.args, query=True), "transform", queries, is_query_inference=True)
         
-        print("***** inference of %d queries *****" % len(queries))
-        dev_query_embedding, dev_query_embedding2id = StreamInferenceDoc(self.args, self.model, GetProcessingFn(
-             self.args, query=True), "transform", queries, is_query_inference=True)
         
-        
-        print("***** faiss search for %d queries on %d shards *****" % (len(queries), self.segments))
+        print("***** faiss search for %d queries on %d shards *****" % (len(topics), self.segments))
         rtr = []
         for i, offset in enumerate(tqdm(self.shard_offsets, unit="shard")):
             scores, neighbours = self.cpu_index[i].search(dev_query_embedding, self.num_results)
@@ -265,9 +277,8 @@ class ANCERetrieval(TransformerBase):
         return pd.DataFrame(rtr, columns=["qid", "query", "docid", "docno", "rank", "score"])
 
 
-class ANCETextScorer(TransformerBase):
-
-    def __init__(self, checkpoint_path=None, text_field='text'):
+class _ANCEModelBase(TransformerBase):
+    def __init__(self, checkpoint_path=None):
         self.args = type('', (), {})()
         args = self.args
         args.local_rank = -1
@@ -280,15 +291,26 @@ class ANCETextScorer(TransformerBase):
         args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
         args.n_gpu = torch.cuda.device_count()
 
-        config, tokenizer, model = load_model(self.args, checkpoint_path)
+        config, tokenizer, model = _load_model(self.args, checkpoint_path)
         self.model = model
         self.tokenizer = tokenizer
+
+
+class ANCETextScorer(_ANCEModelBase):
+
+    def __init__(self, text_field='text', **kwargs):
+        super().__init(**kwargs)        
         self.text_field = text_field
 
     def __str__(self):
         return "ANCETextScorer"
 
     def transform(self, df):
+        assert self.text_field in df.columns
+        assert "docno" in df.columns
+        assert "query" in df.columns
+        assert "qid" in df.columns
+
         queries=[]
         docs = []
         idx_by_query = {}
@@ -337,3 +359,162 @@ class ANCETextScorer(TransformerBase):
         scores = (query_embeddings * passage_embeddings).sum(axis=1)
 
         return df.assign(score=scores)
+
+
+class ANCEQueryEncoder(_ANCEModelBase):
+
+    def __str__(self):
+        return "ANCEQueryEncoder"
+
+    def transform(self, df):
+        assert "docno" not in df.columns
+        assert "query" in df.columns
+        assert "qid" in df.columns
+
+        queries=[]
+        idx_by_query = {}
+        query_idxs = []
+        # We do not want to redo the calculation of query representations, but due to logging
+        # in the ance package, doing a groupby or pt.apply.by_query here will result in
+        # excessive log messages. So we instead calculate each query rep once and keep track of
+        # the correspeonding index so we can project back out the original sequence
+        for q in df["query"].to_list():
+            if q in idx_by_query:
+                query_idxs.append(idx_by_query[q])
+            else:
+                passage = self.tokenizer.encode(
+                    q,
+                    add_special_tokens=True,
+                    max_length=self.args.max_seq_length,
+                )
+                    
+                passage_len = min(len(passage), self.args.max_query_length)
+                input_id_b = pad_input_ids(passage, self.args.max_query_length)
+                queries.append([passage_len, input_id_b])
+                qidx = len(idx_by_query)
+                idx_by_query[q] = qidx
+                query_idxs.append(qidx)
+
+       
+        query_embeddings, _ = StreamInferenceDoc(self.args, self.model, GetProcessingFn(
+             self.args, query=True), "transform", queries, is_query_inference=True)
+
+        # project out the query representations (see comment above)
+        query_embeddings = query_embeddings[query_idxs]
+
+        return df.assign(query_emb=query_embeddings)
+
+class ANCETextEncoder(_ANCEModelBase):
+
+    def __init__(self, text_field='text', **kwargs):
+        super().__init__(**kwargs)        
+        self.text_field = text_field
+
+    def __str__(self):
+        return "ANCETextEncoder"
+
+    def transform(self, df):
+        assert self.text_field in df.columns
+        assert "docno" in df.columns
+        docs = []
+        for d in df[self.text_field].to_list():
+            passage = self.tokenizer.encode(
+                d,
+                add_special_tokens=True,
+                max_length=self.args.max_seq_length,
+            )
+                
+            passage_len = min(len(passage), self.args.max_seq_length)
+            input_id_b = pad_input_ids(passage, self.args.max_seq_length)
+            docs.append([passage_len, input_id_b])
+
+        passage_embeddings, _ = StreamInferenceDoc(self.args, self.model, GetProcessingFn(
+            self.args, query=False), "transform", docs, is_query_inference=False)
+        print(passage_embeddings.shape)
+        rtr = df.copy()
+        rtr["doc_emb"] = passage_embeddings
+        return rtr
+
+class DotScorer(TransformerBase):
+    def transform(self, df):
+        assert "docno" in df.columns
+        assert "query_embs" in df.columns
+        assert "doc_embs" in df.columns
+
+        import torch
+        all_scores = []
+        for qid, group in df.groupby("qid"):
+            query_embeddings = df.iloc[0].query_emb
+            passage_embeddings = torch.stack(df.doc_embs.values.tolist())
+            scores = (query_embeddings * passage_embeddings).sum(axis=1).tolist()
+            all_scores.extend(scores)
+
+        return df.assign(score=all_scores)
+
+
+class PRF_VectorAverage(TransformerBase):
+
+    def __init__(self, alpha = 0, beta=1, k=3):
+        self.beta = beta
+        self.alpha = alpha
+        self.k = k
+
+    def transform(self, df):
+        assert "docno" in df.columns
+        assert "query_embs" in df.columns
+        assert "doc_embs" in df.columns
+
+        new_qids = []
+        new_query_embs = []
+        for qid, group in df.groupby("qid"):
+            k = min(self.k, len(group))
+            all_candidate_embs = torch.stack( group.sort_values("rank").head(k).doc_embs.values.tolist())
+            new_qids.append(qid)
+            new_query_embs.append( (self.alpha) * group.iloc[0].query_embs + self.beta *  torch.mean(all_candidate_embs, axis=0))
+        
+        qembs_df = pd.DataFrame(data={'qid' : new_qids, 'query_emb' : new_query_embs})
+        rtr = df[["qid", "query"]].drop_duplicates().merge(qembs_df, on='qid')
+        return rtr
+
+class ANCEPRF(_ANCEModelBase):
+
+    def __init__(self, k=3, text_field='text', **kwargs):
+        super().__init__(**kwargs)
+        self.k = k
+        self.text_field = text_field
+
+    def transform(self, df):
+        print(df.columns)
+        assert "docno" in df.columns
+        assert "qid" in df.columns
+        assert "query" in df.columns 
+        assert self.text_field in df.columns
+
+        new_qids = []
+        new_query_embs = []
+        for qid, group in df.groupby("qid"):
+            k = min(self.k, len(group))
+            passage_texts = group.sort_values("rank").head(k)[self.text_field].values
+            #this line from pyserini
+            full_text = f'{self.tokenizer.cls_token}{self.tokenizer.sep_token.join(passage_texts)}{self.tokenizer.sep_token}'
+            new_qmeb = self.encode(full_text)
+
+            new_qids.append(qid)
+            new_query_embs.append( new_qmeb )
+        
+        qembs_df = pd.DataFrame(data={'qid' : new_qids, 'query_emb' : new_query_embs})
+        rtr = df[["qid", "query"]].drop_duplicates().merge(qembs_df, on='qid')
+        return rtr
+
+    def encode(self, prf_query : str):
+        inputs = self.tokenizer(
+            [prf_query],
+            max_length=512,
+            padding='longest',
+            truncation=True,
+            add_special_tokens=False,
+            return_tensors='pt'
+        )
+        #inputs.to(self.device)
+        embeddings = self.model(inputs["input_ids"], inputs["attention_mask"]).detach().cpu()
+        return embeddings.flatten()
